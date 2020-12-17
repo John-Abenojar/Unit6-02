1b1ed8e630f0f97d99481a32a1b4885d13065642
JohnAbenojar <john.kirby.abenojar@ocsbstudent.ca>
2020-12-17 23:50:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+number = prompt("Pick a number?");
+if (number > 0){
+  alert("Your number is a positive integer");
+}
+else if (number == 0){
+  alert("Your number is neither positive or negative");
+}
+else{
+  alert("Your number is a negative integer");
+}
